516ec948dc58cde63e0c0a33c189c763e9208349
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-01-14 12:01:24 +0530

    Mongodb connection and import .env in index.js using import method

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+const connectDB =  async () => {
+    try {
+        const connectionInst = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB contected !! DB Host:: ${connectionInst.connection.host}`);
+    } catch (error) {
+        console.log("MongodB connection Error: ", error);
+        process.exit(1)
+    }
+}
+export default connectDB
