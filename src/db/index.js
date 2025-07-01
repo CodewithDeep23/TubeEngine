1a399a28b8587d7922d050b2f363580b9dd85c5e
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-07-01 10:12:13 +0530

    Add Dockerfile and wait-for-it script; create .dockerignore for environment setup

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,7 @@ const connectDB =  async () => {
     try {
         const connectionInst = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB contected !! DB Host:: ${connectionInst.connection.host}`);
+        console.log("Mongo URI:", `${process.env.MONGODB_URI}/${DB_NAME}`);
     } catch (error) {
         console.log("MongodB connection Error: ", error);
         process.exit(1)
